265af7d8fb9e45db4d73e0d72f80ce91cb0360b1
Charles <charlesarchy09@gmail.com>
2022-10-13 14:01:26 -0500

    center login page

--- Components/Login.js
+++ Components/Login.js
@@ -4,14 +4,14 @@ import {signIn} from 'next-auth/react'
 
 function Login() {
   return (
-    <div className="grid place-items-center mt-[250px]">
+    <div className="centerall">
       <Image
         src="https://res.cloudinary.com/chuckmaster/image/upload/v1665374690/facebook%20clone%20files/Facebook-logo_dcs8pi.png"
         height={250}
         width={400}
         ObjectFit="contain"
       />
-      <h1 onClick={signIn} className="p-5 bg-blue-500 rounded-full text-white text-center cursor-pointer"> Login with Facebook</h1>
+      <h1 onClick={signIn} className="centerallfacebook"> Login with Facebook</h1>
 
     </div>
   )
